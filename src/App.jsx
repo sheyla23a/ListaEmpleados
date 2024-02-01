0df8f842ad160515bebb6cf873282d2c24ff033c
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-01 17:22:17 -0300

    agregue los componentes empleado avatar, empleadoLista,empleadoRow y la carpeta utilities con el array de empleados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <>
-  <Container className="bg-secondary text-center mt-3">
-    <h1>Lista de empleados</h1>
-  </Container>
-  </>;
+  return <></>;
 }
 
 export default App;
