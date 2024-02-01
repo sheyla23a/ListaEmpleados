68ddc55001c7378b2f079c6cc4db6aeeeea1ba28
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-01 20:52:23 -0300

    importe a app la lista de empleados y empleados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import EmpleadoList from "./components/EmpleadoList";
+import { Empleados } from "./utilities/Empleados";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <section className=" mt-4  p-2">
       <h1 className="text-center mb-4">Lista de Empleados</h1>
+      <EmpleadoList empleados={Empleados} />
     </section>
   );
 }
