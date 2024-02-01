ca8db0811ca19f428540279bbec000f645b21ac4
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-01 15:59:49 -0300

    agregue la carpeta components y la carperta img con los avatares de los empleados

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+  <Container className="bg-secondary text-center mt-3">
+    <h1>Lista de empleados</h1>
+  </Container>
+  </>;
 }
 
 export default App;
