28686539a1580aac17293ad57ab6391ecd9eba9d
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-02 09:53:51 -0300

    realice modificaciones en la lista de empleados y empleado avatar para solucionar el modelo responsive

--- src/components/EmpleadoAvatar.jsx
+++ src/components/EmpleadoAvatar.jsx
@@ -2,7 +2,7 @@
 const EmpleadoAvatar = ({ pic, fullName }) => {
   return (
     <div className="text-center w-25 ">
-      <div className="card bg-danger">
+      <div>
       <img src={(`../src/img/${pic}`)} alt={fullName} className="img-fluid rounded-circle" />
       </div>
     </div>
--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -5,18 +5,20 @@ const EmpleadoList = ({ empleados }) => {
   return (
     <div className="row">
       {empleados.map((empleado) => (
-        <div key={empleado.id} className="col-lg-4 col-md-4 mb-2">
-          <div className="card d-flex flex-row">
-            <EmpleadoAvatar
-              pic={`empleado${empleado.id}.png`}
-              fullName={empleado.fullName}
-            />
-            <div>
-              <EmpleadoRow
+        <div key={empleado.id} className="col-lg-4 col-md-6 mb-4">
+          <div className="card">
+            <div className="card-body d-flex flex-row w-100">
+              <EmpleadoAvatar
+                pic={`empleado${empleado.id}.png`}
                 fullName={empleado.fullName}
-                title={empleado.title}
-                department={empleado.department}
               />
+              <div className="ms-3">
+                <EmpleadoRow
+                  fullName={empleado.fullName}
+                  title={empleado.title}
+                  department={empleado.department}
+                />
+              </div>
             </div>
           </div>
         </div>
